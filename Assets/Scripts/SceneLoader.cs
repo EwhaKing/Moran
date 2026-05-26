38ec0cc5b0d35926869bcbc732778e2e55a3f826
amyjeong1022-coder <amyjeong1022@ewha.ac.kr>
2026-05-26 16:11:14 +0900

    Untill Narration Scene Sound

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneLoader : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip clickSound;
+
     public void LoadGameScene()
     {
+        StartCoroutine(LoadSceneWithSound());
+    }
+
+    IEnumerator LoadSceneWithSound()
+    {
+        audioSource.PlayOneShot(clickSound);
+
+        yield return new WaitForSeconds(0.3f);
+
         SceneManager.LoadScene("Yoonchae Testscene");
     }
 }
